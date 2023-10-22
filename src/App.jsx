bfc8094c010e78b1495218901499a12f6d8050c1
kapil <khatrikapil987@gmail.com>
2023-10-22 13:16:27 +0530

    search component added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from "./components/Home"
+
+function App() {
+
+  return (
+    <>
+      <div className='mb-8'>
+        <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState();
+  const handleQuery = async (e) => {
+    setQuery(e.target.value);
+    if(!query)
+    {
+      setResult(null)
+    }
+  };
+  const fetchData = async (query) => {
+    const { data } = await axios.get(
+      `http://hn.algolia.com/api/v1/search?query=${query}`
+    );
+    setResult(data);
+  };
+  useEffect(() => {
+    if (query.trim()) {
+      fetchData(query.trim());
+    }
+  }, [query]);
+  return (
+    <>
+      <div className="w-full mt-40 flex flex-col justify-center items-center">
+        <h1 className="text-4xl text-slate-300 mb-8">Search Hacker News</h1>
+        <div className="w-full flex justify-center flex-col items-center">
+          <form className="w-full flex items-center justify-center" action="">
+            <input
+              className="w-2/5 h-12 outline-none p-2 rounded-md bg-slate-300 text-slate-900"
+              value={query}
+              type="text"
+              placeholder="enter the topic"
+              onChange={handleQuery}
+            />
+            <button
+              className="w-20 h-12 rounded-md outline-none bg-gradient-to-bl from-gray-700 via-purple-900 to-blue-900 text-slate-300"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+          {query && result && <div className="w-2/5 p-4 mr-20 bg-[#170640] rounded-md text-slate-400">
+            <ul className="h-72 overflow-auto">
+              {result?.hits?.map((item) => (
+                <li className="min-h-8 flex items-center mb-4">
+                  <img className="h-6 mr-2" src="/images/search.svg" alt="" />{item.title}
+                </li>
+              ))}
+            </ul>
+          </div>}
+        </div>
+      </div>
+    </>
+  );
+}
